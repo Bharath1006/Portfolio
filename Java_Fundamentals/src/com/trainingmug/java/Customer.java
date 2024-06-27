dfb5a3c705a6c1179ac03450726b0d51e58b699a
Bharath Kumar <bharath.p1600@gmail.com>
2024-06-27 17:01:41 -0400

    Pushing from IDE

---
+++ Java_Fundamentals/src/com/trainingmug/java/Customer.java
@@ -0,0 +1,41 @@
+package com.trainingmug.java;
+
+public class Customer {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Bharath Kumar!!!");
+        byte age = 25;
+        System.out.println("Age : " +age);
+
+        String Phone;
+        Phone = "838200717";
+        System.out.println("Phone Number : " + Phone);
+
+        int salary;
+        salary = 280;
+        System.out.println("Salary : " +salary);
+
+        short Shortvalue;
+        Shortvalue = 2500;
+        System.out.println("Short  value : " + Shortvalue);
+
+        long LongValue;
+        LongValue = 9912919554L;
+        System.out.println("LongValue : " + LongValue);
+
+        float FloatValue;
+        FloatValue=101.f;
+        System.out.println("FloatValue : " +FloatValue);
+
+        double DoubleValue;
+        DoubleValue = 154515.6516;
+        System.out.println("DoubleValue : " + DoubleValue);
+
+        boolean BooleanValue;
+        BooleanValue = true;
+        System.out.println("BooleanValue : " + BooleanValue );
+
+
+    }
+}
---
+++ Java_Fundamentals/src/com/trainingmug/java/TypeCasting.java
@@ -0,0 +1,16 @@
+package com.trainingmug.java;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+        System.out.println("This is typecasting");
+
+        int Integervalue;
+        Integervalue = 250;
+        System.out.println("Integervalue : " +Integervalue);
+
+        byte ByteValue;
+        ByteValue = (byte) Integervalue;
+        System.out.println("ByteValue :" +ByteValue);
+    }
+}
